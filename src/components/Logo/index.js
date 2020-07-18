1c25a89fdb5ba273471fed330f103aac765fd6bb
Bruce Muthomi Guantai <muthomiguantai@gmail.com>
2020-07-18 14:41:20 +0300

    designing front-end

---
+++ src/components/Logo/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./style.css";
+
+import { Link } from "react-router-dom";
+
+const Logo = (props) => {
+  return (
+    <Link to="/">
+      <div className="Logo" {...props}>
+        {/* <img src="./logo/logo.png" /> */}
+        <span>Farmers Center Meru</span>
+      </div>
+    </Link>
+  );
+};
+
+export default Logo;
